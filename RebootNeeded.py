d9f99894256a3e1d5b4792e42884bacfacad5d4e
hun7er-py <92925663+hun7er-py@users.noreply.github.com>
2021-10-21 21:02:37 +0000

    Update RebootNeeded.py

--- RebootNeeded.py
+++ RebootNeeded.py
@@ -2,44 +2,30 @@
 #Copyright Hun7er
 #Github.com/hun7er-py
 
-
-#importing necessary libs
-import winreg
+import winreg													#importing necessary libs
 import ctypes
-
-#defining the path where our keys are
-keys = [r"SOFTWARE\WOW6432Node\Portima\ASWeb\Plugins\Update",r"SOFTWARE\WOW6432Node\Portima\PortimaUpd"]
-
-#defining the keys in an array of strings
-key_val = ["RebootNeeded", "Reboot.Needed"]
-
-#replace 2 key values, so for loop with 2 iterations
-for i in range(2):
-
-	#take first value
-	reg_key = keys[i]
-
-	print (reg_key) #debugging
-
-	#calling connection to the reg key p1
-	with winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE) as hkey:
-
-		# now we're opening the key with our hkey, reg_key (from array) and this value we're putting in sub_key
-		with winreg.OpenKey(hkey, reg_key, 0, winreg.KEY_ALL_ACCESS) as sub_key:
-
-			#trying to write the value [i] into key [i]	
+keys = [r"SOFTWARE\WOW6432Node\Portima\ASWeb\Plugins\Update",r"SOFTWARE\WOW6432Node\Portima\PortimaUpd"] 	#defining the path where our keys are
+key_val = ["RebootNeeded", "Reboot.Needed"]									#defining the keys in an array of strings
+for i in range(2):											#replace 2 key values, so for loop with 2 iterations
+	reg_key = keys[i]										#take first value
+	print (reg_key) 										#debugging
+	with winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE) as hkey: 				#calling connection to the reg key p1
+		with winreg.OpenKey(hkey, reg_key, 0, winreg.KEY_ALL_ACCESS) as sub_key:	# now we're opening the key with our hkey, reg_key (from array) and									# this value we're putting in sub_key
+												# trying to write the value [i] into key [i]	
 			try:
 				#key = reg.OpenKey(hkey, reg_key, 0, reg.KEY_ALL_ACCESS)
-				print("Key opened") 											#debugging
-				print("trying to write to key")									#debugging
-				winreg.SetValueEx(sub_key, key_val[i], 0, winreg.REG_DWORD, 0)	#actually writing value
-				print("Writing done...")										#debugging
-				print("trying to close key")									#debugging
-				winreg.CloseKey(sub_key)										#closing and saving key
-				print("Key closed")												#debugging
+				print("Key opened") 							#debugging
+				print("trying to write to key")						#debugging
+				winreg.SetValueEx(sub_key, key_val[i], 0, winreg.REG_DWORD, 0)		#actually writing value
+				print("Writing done...")						#debugging
+				print("trying to close key")						#debugging
+				winreg.CloseKey(sub_key)						#closing and saving key
+				print("Key closed")							#debugging
 			except Exception as e:
-				print(e)														#if exception, print it
+				print(e)								#if exception, print it
 
+else: #if not using iterationm comment this out and place it behind the code block at eof
+	print("Done!")									#debugging
 
 #=================================================================
 #=====  FOLLOWING BLOCK OPTIONAL IN CASE OF NO ITERATION USAGE ===
@@ -63,10 +49,6 @@ for i in range(2):
 				print(e)
 """
 #============================== END BLOCK =========================
-
-else:
-	print("Done!")#debugging
-
 """HWND_BROADCAST = 0xFFFF
 WM_SETTINGCHANGE = 0x1A
 SMTO_ABORTIFHUNG = 0x0002
